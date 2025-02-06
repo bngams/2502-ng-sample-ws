140013753a5b53c52099d41e4c1f344f92c362be
bngams <n.boris@devidia.net>
2025-02-06 12:37:10 +0000

    load children on standalone components

--- apps/fo/src/app/app.routes.ts
+++ apps/fo/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { catalogRoutes } from './features/catalog/catalog.routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
-  { path: 'catalog', children: catalogRoutes },
+  { path: 'catalog', loadChildren: () => import('./features/catalog/catalog.routes').then(m => m.catalogRoutes) },
   { path: '**', component: NotFoundComponent }
 ];
